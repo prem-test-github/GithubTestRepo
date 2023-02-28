8cbe51e9d1b079655ec2b93637e361b924cfa02a
Prem Balasubramanian <prem.bkp@zohocorp.com>
2023-02-28 13:40:27 +0530

    Updating Content
    Enhanced Features

--- .github/actions/CliqInformer/src/CliqInformer.java
+++ .github/actions/CliqInformer/src/CliqInformer.java
@@ -34,13 +34,16 @@ public class CliqInformer {
 			String CliqChannelLink = args[0];		  
 			if(CliqChannelLink.contains("message") && CliqChannelLink.contains("https://cliq.zoho") && CliqChannelLink.contains("/api/v2/") && CliqChannelLink.contains("?zapikey="))
 			  INVALID_ENDPOINT_ERROR = false;
-			String Event = args[1];
+			String CustomMessage = args[1]
+			String[] AddedInfo = args[2].split("_+_");
+			String Repository = AddedInfo[0];
+			String Event = AddedInfo[1];
 			String[] EventWords = Event.split("_");
 			Event = new String();
 			for(String s: EventWords)
 			  Event += s.substring(0,1).toUpperCase() + s.substring(1) + " ";
 			Event = Event.trim();
-			String Action = args[2];
+			String Action = AddedInfo[2];
 			if(!Action.equals(""))
 			{
 			  String[] ActionWords = Action.split("_");
@@ -49,44 +52,18 @@ public class CliqInformer {
 			    Action += s + " ";
 			  Action = Action.trim();
 			}
-			String ServerURL = args[3];
-			String Repository = args[4];
-			String Workflow = args[5];
-			String Actor = args[6];
-			String RunId = args[7];
-			String Ref = args[8];
-			String RefType = args[9];
-			String ActorURL = ServerURL + "/" + Actor;
-			String RepositoryURL = ServerURL + "/" + Repository;
-			String WorkflowURL = RepositoryURL + "/actions/runs/" + RunId;
-			String RefURL = RepositoryURL;
-			if(Ref.contains("pull"))
-			  RefURL = RefURL + "/pull/";
-			else
-			  RefURL = RefURL + "/tree/";
-			if(Ref.split("/").length > 2)
-			  Ref = Ref.split("/")[2];
-			RefURL = RefURL + Ref;
-			String CliqInformerURL = "https://workdrive.zohoexternal.com/external/047d96f793983933bbdb59deb9c44f5443b83a7188e278736405d4d733923181/download?directDownload=true";
 			if(Action.equals(""))
-        Action = "made";
-			CustomMessage = args[10];
-			message = CustomMessage;
-			message = message.replace("(me)","[" + Actor + "](" + ActorURL + ")");
-			message = message.replace("(workflow)","[" + Workflow + "](" + WorkflowURL + ")" );
-			message = message.replace("(repo)","[" + Repository + "](" + RepositoryURL + ")" );
-			message = message.replace("(event)","*" + Event + "*");
-			message = message.replace("(action)",Action);
-			message = message.replace("(ref)",RefType + " [" + Ref + "](" + RefURL + ")" );
-			if(message.length() > MAX_MESSAGE_LENGTH)
+        		Action = "made";
+			String CliqInformerURL = "https://workdrive.zohoexternal.com/external/047d96f793983933bbdb59deb9c44f5443b83a7188e278736405d4d733923181/download?directDownload=true";
+			//message = CustomMessage;
+			message = new String();
+			if(Event == "Push")
 			{
-			  message = CustomMessage;
-			  message = message.replace("(me)","*" + Actor + "*");
-			  message = message.replace("(workflow)","*" + Workflow + "*" );
-			  message = message.replace("(repo)","*" + Repository + "*" );
-			  message = message.replace("(event)","*" + Event + "*");
-			  message = message.replace("(action)",Action);
-			  message = message.replace("(ref)",RefType + " *" + Ref + "*" );
+				String Pusher = AddedInfo[3];
+				String Branch_Name = AddedInfo[4];
+				String Commit_URL = AddedInfo[5];
+				String Compare_URL = AddedInfo[6];
+				message ="[" + Pusher + "](https://github.com/" + Pusher + ") has pushed a new [code](" + Commit_URL + ") in the branch [" + Branch_Name + "](https://github.com/" + Repository + "/tree/" + Branch_Name + ")\n[View Comparison](" + Compare_URL + ")";
 			}
 			ArrayList<String> messages = new ArrayList<String>();
 			for(int i = 0 ; i < message.length() ;)
@@ -124,7 +101,7 @@ public class CliqInformer {
 			for(String msg : messages)
 			{
 			  msg = msg.replace("\"","'");
-			  String TextParams = "{\n\"text\":\"" + msg + "\",\n\"bot\":\n{\n\"name\":\"CliqInformer\",\n\"image\":\"" + CliqInformerURL + "\"\n}}\n";
+			  String TextParams = "{\n\"text\":\"" + msg + "\",\n\"bot\":\n{\n\"name\":\"CliqInformer\",\n\"image\":\"" + CliqInformerURL + "\"\n}}";
 			  connection = (HttpURLConnection) new URL(CliqChannelLink).openConnection();
 			  connection.setRequestMethod("POST");
 			  connection.setRequestProperty("Content-Type","application/json");
@@ -154,6 +131,40 @@ public class CliqInformer {
 			  if(status != 204)
 			    ERROR_MESSAGE = responseContent.toString();
 			}
+			/*String ServerURL = args[3];
+			String Workflow = args[5];
+			String Actor = args[6];
+			String RunId = args[7];
+			String Ref = args[8];
+			String RefType = args[9];
+			String ActorURL = ServerURL + "/" + Actor;
+			String RepositoryURL = ServerURL + "/" + Repository;
+			String WorkflowURL = RepositoryURL + "/actions/runs/" + RunId;
+			String RefURL = RepositoryURL;
+			if(Ref.contains("pull"))
+			  RefURL = RefURL + "/pull/";
+			else
+			  RefURL = RefURL + "/tree/";
+			if(Ref.split("/").length > 2)
+			  Ref = Ref.split("/")[2];
+			RefURL = RefURL + Ref;
+			CustomMessage = args[10];
+			message = message.replace("(me)","[" + Actor + "](" + ActorURL + ")");
+			message = message.replace("(workflow)","[" + Workflow + "](" + WorkflowURL + ")" );
+			message = message.replace("(repo)","[" + Repository + "](" + RepositoryURL + ")" );
+			message = message.replace("(event)","*" + Event + "*");
+			message = message.replace("(action)",Action);
+			message = message.replace("(ref)",RefType + " [" + Ref + "](" + RefURL + ")" );
+			if(message.length() > MAX_MESSAGE_LENGTH)
+			{
+			  message = CustomMessage;
+			  message = message.replace("(me)","*" + Actor + "*");
+			  message = message.replace("(workflow)","*" + Workflow + "*" );
+			  message = message.replace("(repo)","*" + Repository + "*" );
+			  message = message.replace("(event)","*" + Event + "*");
+			  message = message.replace("(action)",Action);
+			  message = message.replace("(ref)",RefType + " *" + Ref + "*" );
+			}*/
 			var githubOutput = System.getenv("GITHUB_OUTPUT");
 			if(Objects.nonNull(githubOutput))
 			    GITHUB_ERROR = false;
